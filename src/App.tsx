45b8c0d866272bdd494823e6cb4e80a09278f4d6
André <andre.katsuragawa@gmail.com>
2021-06-22 16:05:33 -0300

    Add initial pages and Google auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,71 @@
+import { BrowserRouter, Route } from "react-router-dom";
+import { createContext, useState, useEffect } from "react";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { auth, firebase } from "./services/firebase";
+
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
+type AuthContextType = {
+  user: User | undefined;
+  signInWithGoogle: () => Promise<void>;
+};
+
+export const AuthContext = createContext({} as AuthContextType);
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+
+        if (!displayName || !photoURL) {
+          throw new Error("Missing info from Google");
+        }
+
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        });
+      }
+    });
+  }, []);
+
+  async function signInWithGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    const result = await auth.signInWithPopup(provider);
+    if (result.user) {
+      const { displayName, photoURL, uid } = result.user;
+
+      if (!displayName || !photoURL) {
+        throw new Error("Missing info from Google");
+      }
+
+      setUser({
+        id: uid,
+        name: displayName,
+        avatar: photoURL,
+      });
+    }
+  }
+
+  return (
+    <BrowserRouter>
+      <AuthContext.Provider value={{ user, signInWithGoogle }}>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </AuthContext.Provider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
